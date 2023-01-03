0c923548dfff65d4bd4823979b8bbec1fcf52eab
hamomgs <hamomgomer947@gmail.com>
2023-01-03 20:16:41 -0300

    💄 feat: Footer responsive

--- src/components/Footer/styles.jsx
+++ src/components/Footer/styles.jsx
@@ -6,6 +6,20 @@ export const Footer = styled.footer`
   align-items: center;
   gap: 8vw;
   height: 18.45vw;
+
+  @media (max-width: 900px) {
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    width: 92vw;
+    height: 40vw;
+    margin: 5vw auto;
+  }
+  
+  @media (max-width: 600px) {
+    width: 78vw;
+    height: 160vw;
+    margin: 15vw auto;
+  }
 `
 
 export const Container = styled.div`
@@ -13,16 +27,40 @@ export const Container = styled.div`
   flex-direction: column;
   gap: 1.66vw;
   width: ${({ width }) => width ? width : '11.71vw'};
+
+  @media (max-width: 900px) {
+    width: ${({ width }) => width ? '17.8vw' : '16.42vw'};
+  }
+
+  @media (max-width: 600px) {
+    width: ${({ width }) => width ? '36.8vw' : '32.42vw'};
+  }
 `
 
 export const Logo = styled.img`
   width: 11.71vw;
+
+  @media (max-width: 900px) {
+    width: 18vw;
+  }
+
+  @media (max-width: 600px) {
+    width: 35vw;
+  }
 `
 
 export const Title = styled.h3`
   font-family: 'Argentum Bold';
   font-size: 1.46vw;
   color: var(--blue);
+
+  @media (max-width: 900px) {
+    font-size: 2vw;
+  }
+
+  @media (max-width: 600px) {
+    font-size: 4vw;
+  }
 `
 
 export const LinksContainer = styled.ul`
@@ -37,6 +75,22 @@ export const LinksContainer = styled.ul`
 
     &:hover {
       text-decoration: underline;
+    }
+  }
+
+  @media (max-width: 900px) {
+    gap: 0.8vw;
+
+    a {
+      font-size: 1.6vw;
+    }
+  }
+
+  @media (max-width: 600px) {
+    gap: 1.5vw;
+
+    a {
+      font-size: 3.2vw;
     }
   }
 `
